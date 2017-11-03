378c796d9cc2c82bdfc86810d7505bf77ac14e11
hansamali <ham@tiqri.com>
2017-11-03 20:23:22 +0530

    add project files

---
+++ loadingpanel/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace loadingpanel.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult GetData()
+        {
+            List<string> data = new List<string>();
+            for (int i = 0; i < 100000; i++)
+                data.Add(string.Format("name {0}", i));
+            return Json(data);
+        }
+
+    }
+}
